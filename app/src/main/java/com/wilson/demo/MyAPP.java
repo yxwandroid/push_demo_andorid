de7efadc8b99fb24f7f3121499f93056589b2218
杨学武 <yangxuewu@aochuang.com>
2019-07-01 22:01:15 +0800

    first commit

---
+++ app/src/main/java/com/wilson/demo/MyAPP.java
@@ -0,0 +1,77 @@
+package com.wilson.demo;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import com.umeng.commonsdk.UMConfigure;
+import com.umeng.message.IUmengRegisterCallback;
+import com.umeng.message.PushAgent;
+import com.umeng.message.UmengNotificationClickHandler;
+import com.umeng.message.entity.UMessage;
+
+/**
+ * @author : wilson
+ * @project name        :  MultiWeChat
+ * @description :  ${TODO}
+ * @update date         :2019-06-30
+ * @update content      : ${TODO}
+ * ${TAGS}
+ */
+public class MyAPP extends Application {
+    private String TAG = "wilson_push";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        String  AppKey = "5d1817203fc1959372000b4a";
+        String  Secret = "09f28339d1495945701ba1e32dc654b2";
+
+        // 在此处调用基础组件包提供的初始化函数 相应信息可在应用管理 -> 应用信息 中找到 http://message.umeng.com/list/apps
+// 参数一：当前上下文context；
+// 参数二：应用申请的Appkey（需替换）；
+// 参数三：渠道名称；
+// 参数四：设备类型，必须参数，传参数为UMConfigure.DEVICE_TYPE_PHONE则表示手机；传参数为UMConfigure.DEVICE_TYPE_BOX则表示盒子；默认为手机；
+// 参数五：Push推送业务的secret 填充Umeng Message Secret对应信息（需替换）
+        UMConfigure.init(this, AppKey, "Umeng", UMConfigure.DEVICE_TYPE_PHONE, Secret);
+
+
+//获取消息推送代理示例
+        PushAgent mPushAgent = PushAgent.getInstance(this);
+//注册推送服务，每次调用register方法都会回调该接口
+        mPushAgent.register(new IUmengRegisterCallback() {
+
+            @Override
+            public void onSuccess(String deviceToken) {
+                //注册成功会返回deviceToken deviceToken是推送消息的唯一标志
+                Log.i(TAG,"注册成功：deviceToken：-------->  " + deviceToken);
+            }
+
+            @Override
+            public void onFailure(String s, String s1) {
+                Log.e(TAG,"注册失败：-------->  " + "s:" + s + ",s1:" + s1);
+            }
+        });
+
+
+
+
+
+        UmengNotificationClickHandler notificationClickHandler = new UmengNotificationClickHandler(){
+
+
+
+
+            @Override
+            public void dealWithCustomAction(Context context, UMessage msg){
+                Log.i(TAG," msg  ：-------->  " + msg.custom);
+                Log.i(TAG," msg  ：-------->  " + msg.extra.toString());
+            }
+
+        };
+
+        mPushAgent.setNotificationClickHandler(notificationClickHandler);
+
+    }
+}
