10726976e13fab2fef205c19811d07c9424027b3
wilson <yxw_android@163.com>
2019-11-19 16:56:35 +0800

    update

--- app/src/main/java/com/wilson/demo/MyAPP.java
+++ app/src/main/java/com/wilson/demo/MyAPP.java
@@ -10,14 +10,7 @@ import com.umeng.message.PushAgent;
 import com.umeng.message.UmengNotificationClickHandler;
 import com.umeng.message.entity.UMessage;
 
-/**
- * @author : wilson
- * @project name        :  MultiWeChat
- * @description :  ${TODO}
- * @update date         :2019-06-30
- * @update content      : ${TODO}
- * ${TAGS}
- */
+
 public class MyAPP extends Application {
     private String TAG = "wilson_push";
 
@@ -25,10 +18,11 @@ public class MyAPP extends Application {
     public void onCreate() {
         super.onCreate();
 
-        String  AppKey = "5d1817203fc1959372000b4a";
-        String  Secret = "09f28339d1495945701ba1e32dc654b2";
+        String AppKey = "5d1ab3d53fc195d690000a13";
+        String Secret = "5f555515f05f1db2830d5cc23bb89469";
 
-        // 在此处调用基础组件包提供的初始化函数 相应信息可在应用管理 -> 应用信息 中找到 http://message.umeng.com/list/apps
+
+//        // 在此处调用基础组件包提供的初始化函数 相应信息可在应用管理 -> 应用信息 中找到 http://message.umeng.com/list/apps
 // 参数一：当前上下文context；
 // 参数二：应用申请的Appkey（需替换）；
 // 参数三：渠道名称；
@@ -36,37 +30,35 @@ public class MyAPP extends Application {
 // 参数五：Push推送业务的secret 填充Umeng Message Secret对应信息（需替换）
         UMConfigure.init(this, AppKey, "Umeng", UMConfigure.DEVICE_TYPE_PHONE, Secret);
 
-
 //获取消息推送代理示例
         PushAgent mPushAgent = PushAgent.getInstance(this);
+
+
+//        mPushAgent.setResourcePackageName("com.wilson.android.demo");
+
 //注册推送服务，每次调用register方法都会回调该接口
         mPushAgent.register(new IUmengRegisterCallback() {
 
             @Override
             public void onSuccess(String deviceToken) {
                 //注册成功会返回deviceToken deviceToken是推送消息的唯一标志
-                Log.i(TAG,"注册成功：deviceToken：-------->  " + deviceToken);
+                Log.i(TAG, "注册成功：deviceToken：-------->  " + deviceToken);
             }
 
             @Override
             public void onFailure(String s, String s1) {
-                Log.e(TAG,"注册失败：-------->  " + "s:" + s + ",s1:" + s1);
+                Log.e(TAG, "注册失败：-------->  " + "s:" + s + ",s1:" + s1);
             }
         });
 
 
-
-
-
-        UmengNotificationClickHandler notificationClickHandler = new UmengNotificationClickHandler(){
-
-
+        UmengNotificationClickHandler notificationClickHandler = new UmengNotificationClickHandler() {
 
 
             @Override
-            public void dealWithCustomAction(Context context, UMessage msg){
-                Log.i(TAG," msg  ：-------->  " + msg.custom);
-                Log.i(TAG," msg  ：-------->  " + msg.extra.toString());
+            public void dealWithCustomAction(Context context, UMessage msg) {
+                Log.i(TAG, " msg  ：-------->  " + msg.custom);
+                Log.i(TAG, " msg  ：-------->  " + msg.extra.toString());
             }
 
         };
